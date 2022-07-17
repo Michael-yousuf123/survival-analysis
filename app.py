038e4d7470326234275f2f8f56bd8de6ab99625d
miki <miki@miki>
2022-07-17 03:16:47 +0300

    flask app update

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask, render_template, request
 import pickle
 import numpy as np
 from sklearn import preprocessing
+MODELPATH="/home/miki/Desktop/Deployment/survival-analysis/output/model.pkl"
 
-model = pickle.load(open("diabetes.pkl", 'rb'))
+model = pickle.load(open(MODELPATH, 'rb'))
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='/home/miki/Desktop/Deployment/survival-analysis/template')
 app.config['SECRET_KEY'] = "!2345@abc"
 
 @app.route("/")
@@ -16,23 +17,28 @@ def home():
 def predict():
 	try:
 		if request.method == "POST":
-			preg = float(request.form.get('pregnancies'))
-			glucose = float(request.form.get('glucose'))
-			bp = float(request.form.get('bloodpressure'))
-			st = float(request.form.get('skinthickness'))
-			insulin = float(request.form.get('insulin'))
-			bmi = float(request.form.get('bmi'))
-			dpf = float(request.form.get('dpf'))
-			age = float(request.form.get('age'))
-			
-		
-			l = [preg, glucose, bp, st, insulin, bmi, dpf, age]
+			coll = float(request.form.get('collustrum'))
+			exp = float(request.form.get('experience'))
+			ba = float(request.form.get('barn'))
+			own = float(request.form.get("Ownership"))
+			ag = float(request.form.get("age"))
+			wean = float(request.form.get("weaning"))
+			liq = float(request.form.get("liquid"))
+			sex = float(request.form.get("sex"))
+			inc = float(request.form.get("income"))
+			siz = float(request.form.get("size"))
+			edu = float(request.form.get("age"))
+			hou = float(request.form.get("housing"))
+			par = float(request.form.get("parity"))
+			fc = float(request.form.get("feeding collustrum"))
+	
+			l = [coll, exp, ba, own, ag, wean, liq, sex, inc, siz, edu, hou, par, fc]
 			l = np.asarray(l)
-			l = np.reshape(l, (1,8))
+			l = np.reshape(l, (1,12))
 			pred = model.predict(l)
-			return render_template("result.html", prediction = pred[0])
+			return render_template('output.html', prediction = pred[0])
 	except:
-		return render_template("result.html", no_value = 1)
+		return render_template('output.html', no_value = 1)
 
 if __name__ == '__main__':
 	app.run(debug = True)
--- test/test_index.py
+++ test/test_index.py
@@ -1,4 +1,5 @@
 from app import index
+import numpy as np
 
 def test_index():
     assert index() == "Survival Analysis of Kids"
